438bc25c4f942667c233a4c9335e71b9fae223de
Denis Novikov <denverden@mail.ru>
2021-08-10 21:33:22 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.max = 0;
+        this.min = 0;
+        this.half = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.half = Math.ceil((this.min + this.max) / 2);
+        return this.half;
     }
 
     lower() {
-
+        this.max = this.half;
     }
 
     greater() {
-
+        this.min = this.half;
     }
 }
 
